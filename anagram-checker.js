0fa0e85dc9f7b295bb01844f761597f22c192af7
James Serengia <jamesserengia@gmail.com>
2022-10-06 16:08:35 +0300

    Anagram solution

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,3 +1,22 @@
-module.exports = function (str) {
-  // write your code here
-}
+module.exports = function (strOne, strTwo) {
+  // strip off no-alphanumeric
+  // If length of str1 !== length of str2 return false
+  // loop through str1 and check if it's chars are in str2
+  // if char in str1 is not in str2 return false
+  // return true
+
+  const str1 = strOne.toLowerCase().replace(/[^0-9a-z]/gi, "");
+  const str2 = strTwo.toLowerCase().replace(/[^0-9a-z]/gi, "");
+
+  if (str1.length !== str2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < str1.length; i++) {
+    if (!str2.includes(str1[i])) {
+      return false;
+    }
+  }
+
+  return true;
+};
